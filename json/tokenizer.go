03af89d316eaa27a9e9b0c68b808149892fd86d9
Martins Irbe <martins_irbe@mail.com>
2019-02-23 23:09:05 +0000

    Add json input tokenizer

---
+++ json/tokenizer.go
@@ -0,0 +1,228 @@
+package json
+
+import (
+	"fmt"
+	"unicode"
+)
+
+const (
+	// UNKNOWN used when didn't match any of expected token types
+	UNKNOWN TokenType = iota
+	// EOF identifies the end of the input
+	EOF
+	// COLON identifies a colon separator
+	COLON
+	// COMMA identifies a comma separator
+	COMMA
+	// STRING identifies a string literal
+	STRING
+	// INTEGER identifies an integer literal
+	INTEGER
+	// BOOLEAN identifies a boolean literal
+	BOOLEAN
+	// LCBRACKET identifies a left curly bracket separator
+	LCBRACKET
+	// RCBRACKET identifies a right curly bracket separator
+	RCBRACKET
+	// LSBRACKET identifies a left square bracket separator
+	LSBRACKET
+	// RSBRACKET identifies a right square bracket separator
+	RSBRACKET
+
+	colonTokenValue              = ':'
+	commaTokenValue              = ','
+	leftCurlyBracketTokenValue   = '{'
+	rightCurlyBracketTokenValue  = '}'
+	leftSquareBracketTokenValue  = '['
+	rightSquareBracketTokenValue = ']'
+	eofTokenValue                = "eof"
+)
+
+var (
+	// these chars will be skipped during the JSON object scan
+	skipChars         = []rune{'\n', '\t', '\r', ' '}
+	boolTokenValue    string
+	integerTokenValue string
+	stringTokenValue  string
+)
+
+// TokenType defines the type of the token based on JSON syntax
+type TokenType int
+
+// String provides human readable token type string
+func (tt TokenType) String() string {
+	return []string{
+		"unknown",
+		"eof",
+		"colon",
+		"comma",
+		"string",
+		"integer",
+		"boolean",
+		"left curly bracket",
+		"right curly bracket",
+		"left square bracket",
+		"right square bracket",
+	}[tt]
+}
+
+// Token lexically represents a part of the JSON syntax structure
+type Token struct {
+	Value string
+	Type  TokenType
+}
+
+// Tokenizer parses a JSON object and builds a lexical representation of it
+type Tokenizer struct {
+	charsIn     []rune
+	currentChar rune
+	start, end  int
+}
+
+// NewTokenizer initialises a new instance of the tokenizer
+func NewTokenizer(charsIn []rune) *Tokenizer {
+	scanner := Tokenizer{charsIn: charsIn}
+	scanner.scanNextChar()
+	return &scanner
+}
+
+// PeekToken preview the next token from the char input
+func (t *Tokenizer) PeekToken() *Token {
+	start := t.start
+	end := t.end
+	tok := t.GetToken()
+
+	t.start = start
+	t.end = end
+	t.currentChar = t.charsIn[t.end-1]
+
+	return tok
+}
+
+// GetToken get the next token from the char input
+func (t *Tokenizer) GetToken() *Token {
+	for {
+		if !shouldSkipChar(t.currentChar) {
+			break
+		}
+		t.scanNextChar()
+	}
+
+	token := t.getToken()
+
+	t.scanNextChar()
+	return &token
+}
+
+func (t *Tokenizer) getToken() Token {
+	switch {
+	case t.currentChar == colonTokenValue:
+		return Token{Type: COLON, Value: string(colonTokenValue)}
+	case t.currentChar == commaTokenValue:
+		return Token{Type: COMMA, Value: string(commaTokenValue)}
+	case t.currentChar == leftCurlyBracketTokenValue:
+		return Token{Type: LCBRACKET, Value: string(leftCurlyBracketTokenValue)}
+	case t.currentChar == rightCurlyBracketTokenValue:
+		return Token{Type: RCBRACKET, Value: string(rightCurlyBracketTokenValue)}
+	case t.currentChar == leftSquareBracketTokenValue:
+		return Token{Type: LSBRACKET, Value: string(leftSquareBracketTokenValue)}
+	case t.currentChar == rightSquareBracketTokenValue:
+		return Token{Type: RSBRACKET, Value: string(rightSquareBracketTokenValue)}
+	case t.isString():
+		return Token{Type: STRING, Value: stringTokenValue}
+	case t.isInteger():
+		return Token{Type: INTEGER, Value: integerTokenValue}
+	case t.isBoolean():
+		return Token{Type: BOOLEAN, Value: boolTokenValue}
+	case t.currentChar == rune(0):
+		return Token{Type: EOF, Value: eofTokenValue}
+	}
+	return Token{Type: UNKNOWN, Value: string(t.currentChar)}
+}
+
+func (t *Tokenizer) scanNextChar() {
+	t.currentChar = 0
+	if !t.isLastChar() {
+		t.currentChar = t.charsIn[t.end]
+	}
+	t.start = t.end
+	t.end++
+}
+
+func shouldSkipChar(currentChar rune) bool {
+	for _, skipChar := range skipChars {
+		if currentChar == skipChar {
+			return true
+		}
+	}
+	return false
+}
+
+func (t *Tokenizer) getBooleanValue() string {
+	switch {
+	case t.currentChar == 't':
+		return string(t.charsIn[t.start : t.start+4])
+	case t.currentChar == 'f':
+		return string(t.charsIn[t.start : t.start+5])
+	}
+	return ""
+}
+
+func (t *Tokenizer) isLastChar() bool {
+	return t.end >= len(t.charsIn)
+}
+
+func (t *Tokenizer) isBoolean() bool {
+	if t.currentChar != 't' && t.currentChar != 'f' && t.start+5 <= len(t.charsIn) {
+		return false
+	}
+
+	bv := t.getBooleanValue()
+	switch {
+	case bv != "true" && bv != "false":
+		return false
+	case bv == "true":
+		t.end += 4
+	case bv == "false":
+		t.end += 5
+	}
+	t.currentChar = t.charsIn[t.end]
+	boolTokenValue = bv
+	return true
+}
+
+func (t *Tokenizer) isString() bool {
+	if t.currentChar != '"' {
+		return false
+	}
+
+	stringTokenValue = fmt.Sprintf("%c", t.charsIn[t.end])
+
+	for i := t.end + 1; i < len(t.charsIn); i++ {
+		if t.charsIn[i] != '"' {
+			stringTokenValue = fmt.Sprintf("%s%c", stringTokenValue, t.charsIn[i])
+			continue
+		}
+		t.end = i + 1
+		t.currentChar = t.charsIn[t.end]
+		return true
+	}
+	return false
+}
+
+func (t *Tokenizer) isInteger() bool {
+	if t.currentChar != '-' && !unicode.IsDigit(t.currentChar) {
+		return false
+	}
+
+	integerTokenValue = fmt.Sprintf("%c%c", t.currentChar, t.charsIn[t.end])
+
+	t.end++
+	t.currentChar = t.charsIn[t.end]
+	for unicode.IsDigit(t.currentChar) {
+		integerTokenValue = fmt.Sprintf("%s%c", integerTokenValue, t.charsIn[t.end])
+		t.end++
+		t.currentChar = t.charsIn[t.end]
+	}
+	return true
+}
---
+++ json/tokenizer_test.go
@@ -0,0 +1,350 @@
+package json_test
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/martinsirbe/go-astify/json"
+)
+
+func TestCorrectlyTokenizedTestJSON(t *testing.T) {
+	jsonFile, err := os.Open("../test_data/test.json")
+	defer func() {
+		assert.Nil(t, jsonFile.Close())
+	}()
+	assert.Nil(t, err)
+
+	jsonBytes, err := ioutil.ReadAll(jsonFile)
+	assert.Nil(t, err)
+
+	tokenizer := json.NewTokenizer([]rune(string(jsonBytes)))
+	for _, expectedToken := range expectedTokens() {
+		token := tokenizer.GetToken()
+		assert.Equal(t, expectedToken.Type, token.Type)
+		assert.Equal(t, expectedToken.Value, token.Value)
+	}
+}
+
+func TestSuccessfullyPreviewedNextToken(t *testing.T) {
+	jsonFile, err := os.Open("../test_data/test.json")
+	defer func() {
+		assert.Nil(t, jsonFile.Close())
+	}()
+	assert.Nil(t, err)
+
+	jsonBytes, err := ioutil.ReadAll(jsonFile)
+	assert.Nil(t, err)
+
+	tokenizer := json.NewTokenizer([]rune(string(jsonBytes)))
+	nextToken := tokenizer.PeekToken()
+	assert.Equal(t, "{", nextToken.Value)
+	assert.Equal(t, json.LCBRACKET, nextToken.Type)
+}
+
+func TestWhenPreviewingNextTokenDoesNotAdvanceToNextToken(t *testing.T) {
+	jsonFile, err := os.Open("../test_data/test.json")
+	defer func() {
+		assert.Nil(t, jsonFile.Close())
+	}()
+	assert.Nil(t, err)
+
+	jsonBytes, err := ioutil.ReadAll(jsonFile)
+	assert.Nil(t, err)
+
+	tokenizer := json.NewTokenizer([]rune(string(jsonBytes)))
+	nextToken := tokenizer.PeekToken()
+	assert.Equal(t, "{", nextToken.Value)
+	assert.Equal(t, json.LCBRACKET, nextToken.Type)
+
+	nextToken = tokenizer.PeekToken()
+	assert.Equal(t, "{", nextToken.Value)
+	assert.Equal(t, json.LCBRACKET, nextToken.Type)
+}
+
+func expectedTokens() []json.Token {
+	return []json.Token{
+		{
+			Value: "{",
+			Type:  json.LCBRACKET,
+		},
+		{
+			Value: "test_object",
+			Type:  json.STRING,
+		},
+		{
+			Value: ":",
+			Type:  json.COLON,
+		},
+		{
+			Value: "{",
+			Type:  json.LCBRACKET,
+		},
+		{
+			Value: "test_string",
+			Type:  json.STRING,
+		},
+		{
+			Value: ":",
+			Type:  json.COLON,
+		},
+		{
+			Value: "hello world",
+			Type:  json.STRING,
+		},
+		{
+			Value: ",",
+			Type:  json.COMMA,
+		},
+		{
+			Value: "test_integer",
+			Type:  json.STRING,
+		},
+		{
+			Value: ":",
+			Type:  json.COLON,
+		},
+		{
+			Value: "1234567890",
+			Type:  json.INTEGER,
+		},
+		{
+			Value: ",",
+			Type:  json.COMMA,
+		},
+		{
+			Value: "test_boolean",
+			Type:  json.STRING,
+		},
+		{
+			Value: ":",
+			Type:  json.COLON,
+		},
+		{
+			Value: "true",
+			Type:  json.BOOLEAN,
+		},
+		{
+			Value: "}",
+			Type:  json.RCBRACKET,
+		},
+		{
+			Value: ",",
+			Type:  json.COMMA,
+		},
+		{
+			Value: "test_string",
+			Type:  json.STRING,
+		},
+		{
+			Value: ":",
+			Type:  json.COLON,
+		},
+		{
+			Value: "hello world",
+			Type:  json.STRING,
+		},
+		{
+			Value: ",",
+			Type:  json.COMMA,
+		},
+		{
+			Value: "test_integer",
+			Type:  json.STRING,
+		},
+		{
+			Value: ":",
+			Type:  json.COLON,
+		},
+		{
+			Value: "1234567890",
+			Type:  json.INTEGER,
+		},
+		{
+			Value: ",",
+			Type:  json.COMMA,
+		},
+		{
+			Value: "test_boolean",
+			Type:  json.STRING,
+		},
+		{
+			Value: ":",
+			Type:  json.COLON,
+		},
+		{
+			Value: "true",
+			Type:  json.BOOLEAN,
+		},
+		{
+			Value: "test_string_array",
+			Type:  json.STRING,
+		},
+		{
+			Value: ":",
+			Type:  json.COLON,
+		},
+		{
+			Value: "[",
+			Type:  json.LSBRACKET,
+		},
+		{
+			Value: "test 1",
+			Type:  json.STRING,
+		},
+		{
+			Value: ",",
+			Type:  json.COMMA,
+		},
+		{
+			Value: "test 2",
+			Type:  json.STRING,
+		},
+		{
+			Value: ",",
+			Type:  json.COMMA,
+		},
+		{
+			Value: "test 3",
+			Type:  json.STRING,
+		},
+		{
+			Value: "]",
+			Type:  json.RSBRACKET,
+		},
+		{
+			Value: ",",
+			Type:  json.COMMA,
+		},
+		{
+			Value: "test_object_array",
+			Type:  json.STRING,
+		},
+		{
+			Value: ":",
+			Type:  json.COLON,
+		},
+		{
+			Value: "[",
+			Type:  json.LSBRACKET,
+		},
+		{
+			Value: "{",
+			Type:  json.LCBRACKET,
+		},
+		{
+			Value: "test_string",
+			Type:  json.STRING,
+		},
+		{
+			Value: ":",
+			Type:  json.COLON,
+		},
+		{
+			Value: "hello world 1",
+			Type:  json.STRING,
+		},
+		{
+			Value: ",",
+			Type:  json.COMMA,
+		},
+		{
+			Value: "test_integer",
+			Type:  json.STRING,
+		},
+		{
+			Value: ":",
+			Type:  json.COLON,
+		},
+		{
+			Value: "123",
+			Type:  json.INTEGER,
+		},
+		{
+			Value: ",",
+			Type:  json.COMMA,
+		},
+		{
+			Value: "test_boolean",
+			Type:  json.STRING,
+		},
+		{
+			Value: ":",
+			Type:  json.COLON,
+		},
+		{
+			Value: "false",
+			Type:  json.BOOLEAN,
+		},
+		{
+			Value: "}",
+			Type:  json.RCBRACKET,
+		},
+		{
+			Value: ",",
+			Type:  json.COMMA,
+		},
+		{
+			Value: "{",
+			Type:  json.LCBRACKET,
+		},
+		{
+			Value: "test_string",
+			Type:  json.STRING,
+		},
+		{
+			Value: ":",
+			Type:  json.COLON,
+		},
+		{
+			Value: "hello world 2",
+			Type:  json.STRING,
+		},
+		{
+			Value: ",",
+			Type:  json.COMMA,
+		},
+		{
+			Value: "test_integer",
+			Type:  json.STRING,
+		},
+		{
+			Value: ":",
+			Type:  json.COLON,
+		},
+		{
+			Value: "456",
+			Type:  json.INTEGER,
+		},
+		{
+			Value: ",",
+			Type:  json.COMMA,
+		},
+		{
+			Value: "test_boolean",
+			Type:  json.STRING,
+		},
+		{
+			Value: ":",
+			Type:  json.COLON,
+		},
+		{
+			Value: "true",
+			Type:  json.BOOLEAN,
+		},
+		{
+			Value: "}",
+			Type:  json.RCBRACKET,
+		},
+		{
+			Value: "]",
+			Type:  json.RSBRACKET,
+		},
+		{
+			Value: "}",
+			Type:  json.RCBRACKET,
+		},
+	}
+}
